425272407f83ae2d46dab4bd3dfdbc10e57ee567
CesarEspitia <livecesar@hotmail.com>
2020-03-16 00:48:14 -0500

    Library: Data Access configuration SQL - Binding Prize Creation Form

---
+++ TournamentTrackerDB/dbo/Programability/StoredProcedures/spPrizes_GetByTournament.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [dbo].[spPrizes_GetByTournament]
+	@TournamentId int
+	
+AS
+begin
+SET NOCOUNT ON;
+	SELECT p.*
+	from dbo.Prizes p
+	inner join dbo.TournamentPrizes t on p.Id=t.PrizeId
+	where t.TournamentId = @TournamentId
+end
---
+++ TournamentTrackerDB/dbo/Programability/StoredProcedures/spPrizes_Insert.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE [dbo].[spPrizes_Insert]
+	@PlaceNumber int,
+	@PlaceName nvarchar(50),
+	@PrizeAmount money,
+	@PrizePercentage float,
+	@id int = 0 output
+AS
+BEGIN
+SET NOCOUNT ON;
+INSERT INTO dbo.Prizes (PlaceNumber, PlaceName, PrizeAmount, PrizePercentage)
+VALUES (@PlaceNumber, @PlaceName, @PrizeAmount,@PrizePercentage);
+
+	SELECT @id = SCOPE_IDENTITY();
+END
